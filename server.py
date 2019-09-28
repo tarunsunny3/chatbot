7b1a9b2e7fabf907a90ae3260d618b7aa518b0d0
Tarun <tarunsunny3@gmail.com>
2019-09-28 18:59:10 +0530

    Server side script

--- server.py
+++ server.py
@@ -2,7 +2,15 @@
 # import socket
 import socket
 host = "10.5.1.15"
-port = 1000
-server_socket = socketsocket()
+port = 5000
+server_socket = socket.socket()
 server_socket.bind((host,port))
 server_socket.listen()
+conn, address = server_socket.accept() #conn-connection
+print("Connection from : " + str(address))
+while True:
+    data = conn.recv(1024).decode()
+    print("From connected user: " + str(data))
+    data = input(' -> ')
+    conn.send(data.encode())
+conn.close()
